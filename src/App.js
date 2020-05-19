0c1e9bca16314edeea048b348cf7808845817b8f
Lailz <lailaabdulraheem@gmail.com>
2020-05-19 08:11:10 +0300

    add starting point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Data
+import cookies from "./cookies";
+
+// Styling
+import styles from "./styles";
 
 function App() {
+  const cookieList = cookies.map(cookie => (
+    <div style={styles.cookie} key={cookie.id}>
+      <img style={styles.cookieImage} alt={cookie.name} src={cookie.image} />
+      <p style={styles.text}>{cookie.name}</p>
+      <p style={styles.text}>{cookie.price} KD</p>
+    </div>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.body}>
+      <div>
+        <h1 style={styles.text}>Cookies and Beyond</h1>
+        <h4 style={styles.text}>Where cookie maniacs gather</h4>
+        <img
+          alt="cookie shop"
+          src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+          style={styles.shopImage}
+        />
+      </div>
+      <div style={styles.list}>{cookieList}</div>
     </div>
   );
 }
