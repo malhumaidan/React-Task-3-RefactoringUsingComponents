2e3237da3a5f707f2c37e0d5328ce7da586bdf5d
yanalshmilan <yshmilan@gmail.com>
2021-12-20 05:22:32 +0200

    styled to css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React from 'react';
 // Data
-import products from "./products";
+import products from './products';
 // Styling
-import styles from "./styles";
+import './App.css';
 
 function App() {
   const productList = products.map((product) => (
-    <div style={styles.product}>
-      <img style={styles.productImage} alt={product.name} src={product.image} />
-      <p style={styles.text}>{product.name}</p>
-      <p style={styles.text}>{product.price} KD</p>
+    <div className="product">
+      <img className="productImage" alt={product.name} src={product.image} />
+      <p className="text">{product.name}</p>
+      <p className="text">{product.price} KD</p>
     </div>
   ));
   return (
-    <div style={styles.body}>
+    <div className="body">
       <div>
-        <h1 style={styles.text}>Cookies and Beyond</h1>
-        <h4 style={styles.text}>Where cookie maniacs gather</h4>
+        <h1 className="text">Cookies and Beyond</h1>
+        <h4 className="text">Where cookie maniacs gather</h4>
         <img
           alt="cookie shop"
           src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
-          style={styles.shopImage}
+          className="shopImage"
         />
       </div>
-      <div style={styles.list}>{productList}</div>
+      <div className="list">{productList}</div>
     </div>
   );
 }
